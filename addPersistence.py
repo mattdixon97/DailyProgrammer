c8f1bceb63afc355c3853a47ecc55d6426817f92
Matthew Dixon <matthewkdixon@gmail.com>
2019-01-30 11:47:57 -0500

    Added headers to Challenges #372 and #374

--- addPersistence.py
+++ addPersistence.py
@@ -1,3 +1,10 @@
+'''
+Calculate the additive persistence of a number
+http://mathworld.wolfram.com/AdditivePersistence.html
+
+https://www.reddit.com/r/dailyprogrammer/comments/akv6z4/20190128_challenge_374_easy_additive_persistence/
+'''
+
 def persistence(n, p=1):
     s = 0
     while n:
--- balanced.py
+++ balanced.py
@@ -1,3 +1,10 @@
+'''
+Determines if there is the same number of two chars in a string
+Bonus: Determines if every letter in a string appears the same number of times
+
+https://www.reddit.com/r/dailyprogrammer/comments/afxxca/20190114_challenge_372_easy_perfectly_balanced/
+'''
+
 def balanced(s):
     return True if len(s) == 0 else (len(s) / 2) == s.count(s[0])
 
